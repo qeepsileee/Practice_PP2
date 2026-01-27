2b7d461e1874f100745fa1cd306ebbeca1dce054
qeepsileee <baktzhanzamanbek@gmail.com>
2026-01-27 13:22:54 +0500

    Added Practice2 folders and examples

---
+++ Practice2/if_else_elif/if_else_elif.py
@@ -0,0 +1,130 @@
+#Equals: a == b
+#Not Equals: a != b
+#Less than: a < b
+#Less than or equal to: a <= b
+#Greater than: a > b
+#Greater than or equal to: a >= b
+
+
+
+
+
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+
+
+
+  number = 15
+if number > 0:
+  print("The number is positive")
+
+
+
+
+
+  age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+
+
+
+
+
+
+  is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
+
+
+
+
+
+
+#elif
+
+  a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+
+
+
+  score = 75
+
+if score >= 90:
+  print("Grade: A")
+elif score >= 80:
+  print("Grade: B")
+elif score >= 70:
+  print("Grade: C")
+elif score >= 60:
+  print("Grade: D")
+
+
+
+
+  age = 25
+
+if age < 13:
+  print("You are a child")
+elif age < 20:
+  print("You are a teenager")
+elif age < 65:
+  print("You are an adult")
+elif age >= 65:
+  print("You are a senior")
+
+
+
+
+
+
+  day = 3
+
+if day == 1:
+  print("Monday")
+elif day == 2:
+  print("Tuesday")
+elif day == 3:
+  print("Wednesday")
+elif day == 4:
+  print("Thursday")
+elif day == 5:
+  print("Friday")
+elif day == 6:
+  print("Saturday")
+elif day == 7:
+  print("Sunday")
+
+
+
+
+
+#else 
+
+  
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+
+  #Short Hand If 
+  # If you have only one statement to execute, you can put it on the same line as the if statement.
+
+a = 5
+b = 2
+if a > b: print("a is greater than b")
+
