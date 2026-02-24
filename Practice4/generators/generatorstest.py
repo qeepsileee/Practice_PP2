4a11a96722967043a1e7289f805bcbfd139dfc6e
Zamanbek <baktzhanzamanbek@gmail.com>
2026-02-24 13:12:10 +0500

    Refactor even number generation logic
    Refactor even number generation and output.

--- Practice4/generators/generatorstest.py
+++ Practice4/generators/generatorstest.py
@@ -10,11 +10,13 @@ for num in square_generator(5):
 
 
 
-    n = int(input("Enter n: "))
+    n = int(input())
 
-even_gen = (i for i in range(n + 1) if i % 2 == 0)
-
-print(",".join(str(num) for num in even_gen))
+for i in range(0, n + 1, 2):
+    if i != n and n % 2 == 0:
+        print(i, end=",")
+    else:
+        print(i)
 
 
 
