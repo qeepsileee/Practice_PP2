6a8838de67819b60e418f206f6151a69a9a92384
qeepsileee <baktzhanzamanbek@gmail.com>
2026-03-03 12:10:20 +0500

    Add Practice5 - Python RegEx and receipt parsing examples

--- Practice5/receipt_parser.py
+++ Practice5/receipt_parser.py
@@ -23,3 +23,81 @@ pattern = r"[a-z]_+[a-z]+"
 text = "hello_world test_string Invalid_String"
 match = re.findall(pattern,text)
 print(match)
+
+
+
+
+import re
+pattern = r"[A-Z][a-z]+"
+text = "Hello World TEST Python"
+match = re.findall(pattern , text)
+print(match)
+
+
+
+import re
+pattern = r"^a.*b$"
+text = "axxxb"
+match = re.fullmatch(pattern, text)
+print(match)
+
+
+
+
+import re
+
+text = "Hello, world. Python is fun"
+result = re.sub(r"[ ,\.]", ":", text)
+
+print(result)
+
+
+
+
+import re
+
+text = "hello_world_python"
+
+result = re.sub(r"_([a-z])", lambda match: match.group(1).upper(), text)
+
+print(result)
+
+
+
+
+import re
+pattern = r"(?=[A-Z])"
+text = 'HelloWorldPython'
+match = re.split(pattern , text)
+print(match)
+
+
+
+
+
+
+
+import re
+
+text = "HelloWorldPython"
+result = re.sub(r"(?<!^)(?=[A-Z])", " ", text)
+
+print(result)
+
+
+
+
+
+
+
+
+import re
+
+def camel_to_snake(text):
+    return re.sub(r"(?<!^)(?=[A-Z])", "_", text).lower()
+
+text = "helloWorldPython"
+print(camel_to_snake(text))
+
+
+
